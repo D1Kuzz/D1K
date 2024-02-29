51b0669403bf7afb6d63686da979baae5fe020d2
Dmitrii <zaxa157@gmail.com>
2024-02-29 09:37:09 +0700

    Добавьте файлы проекта.

---
+++ Z2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Prog
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Числа от 100 до 200, которые кратны трем");
+            for (int i = 100; i <= 200; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+            int a = 50;
+            int b = 150;
+            int c = 5;
+
+            for (int i = a; i <= b; i++)
+            {
+                if (i % c == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
